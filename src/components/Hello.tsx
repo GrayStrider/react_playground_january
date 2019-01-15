9e80001cebbcab7eb5de4bb52f8130b5f4785b74
GrayStrider <GrayStrider@users.noreply.github.com>
2019-01-15 16:29:30 +1100

    Added Underscore.js

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
+import * as _ from "underscore";
 import '../styles/Hello.css';
-
 export interface Props {
     name: string;
     enthusiasmLevel?: number;
@@ -21,6 +21,9 @@ function Hello({name, enthusiasmLevel = 1, onIncrement, onDecrement}: Props) {
             <div>
                 <button onClick={onDecrement}>-</button>
                 <button onClick={onIncrement}>+</button>
+            </div>
+            <div>
+                <p>{_.range(1, 11, 1)}</p>
             </div>
         </div>
     );
